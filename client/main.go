ca8d20b59033e9b20157c6aa8b85399b0d06eef4
Alexandre <alexandre@endor.ai>
2024-09-20 09:40:39 -1000

    demo

--- client/main.go
+++ client/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"github.com/ekkinox/bazel-demo/common"
-	pb "github.com/ekkinox/bazel-demo/proto/calculator"
+	// pb "github.com/ekkinox/bazel-demo/proto/calculator"
 	"google.golang.org/grpc"
 )
 
@@ -13,26 +12,26 @@ func main() {
 
 	log.Println("Starting gRPC client on :50051 ...")
 
-	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	_, err := grpc.Dial(":50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
 
-	c := pb.NewCalculatorServiceClient(conn)
+	// c := pb.NewCalculatorServiceClient(conn)
 
-	res, err := c.Calculate(context.Background(), &pb.OperationRequest{
-		Operation: &pb.Operation{
-			Operator: pb.Operator_OPERATOR_PRODUCT,
-			Number1:  2,
-			Number2:  3,
-		},
-	})
+	// res, err := c.Calculate(context.Background(), &pb.OperationRequest{
+	// 	Operation: &pb.Operation{
+	// 		Operator: pb.Operator_OPERATOR_PRODUCT,
+	// 		Number1:  2,
+	// 		Number2:  3,
+	// 	},
+	// })
 
-	if err != nil {
-		log.Fatalf("Error: %v", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error: %v", err)
+	// }
 
-	log.Printf("Result: %v", res.Result)
+	// log.Printf("Result: %v", res.Result)
 
 	common.VulnerableFunction()
 }
